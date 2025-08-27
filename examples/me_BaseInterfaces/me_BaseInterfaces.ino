066399bd45c32b87195d7f0786d5aa57ce3552a1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-08-27 21:33:44 +0200

    [+] [Examples] Iterator basic test.

--- examples/me_BaseInterfaces/me_BaseInterfaces.ino
+++ examples/me_BaseInterfaces/me_BaseInterfaces.ino
@@ -7,13 +7,39 @@
 
 #include <me_BaseInterfaces.h>
 
+#include <me_BaseTypes.h>
+#include <me_Console.h>
+
 void CompilationTest()
 {
 }
 
+void IteratorTest()
+{
+  TAddressIterator Rator;
+  TAddress Addr;
+
+
+  Console.Print("( IteratorTest");
+
+  Rator.Init(1, 0);
+
+  while (Rator.GetNextAddr(&Addr))
+  {
+    Console.Write("Addr");
+    Console.Print(Addr);
+    Console.EndLine();
+  }
+
+  Console.Print(") Done");
+}
+
 void setup()
 {
+  Console.Init();
+
   CompilationTest();
+  IteratorTest();
 }
 
 void loop()
