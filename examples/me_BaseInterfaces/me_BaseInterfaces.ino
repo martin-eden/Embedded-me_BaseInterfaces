1abbc0f28f62cdb26918ba7da5e1fc0e3fe1c29b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-08-28 21:32:18 +0200

    [+] [Example] Compilation test, iterator test.

--- examples/me_BaseInterfaces/me_BaseInterfaces.ino
+++ examples/me_BaseInterfaces/me_BaseInterfaces.ino
@@ -10,19 +10,45 @@
 #include <me_BaseTypes.h>
 #include <me_Console.h>
 
+void Func_Method(
+  TAddress Data [[gnu::unused]],
+  TAddress Instance [[gnu::unused]]
+)
+{
+};
+
+TBool Func_Operation(
+  TAddress Data [[gnu::unused]],
+  TAddress Instance [[gnu::unused]]
+)
+{
+  return true;
+};
+
+TBool Func_FixedOperation(
+  TAddress Data [[gnu::unused]]
+)
+{
+  return true;
+};
+
 void CompilationTest()
 {
+  [[gnu::unused]] TMethod Method = Func_Method;
+  [[gnu::unused]] TOperation Operation = Func_Operation;
+  [[gnu::unused]] TFixedOperation FixedOperation = Func_FixedOperation;
 }
 
 void IteratorTest()
 {
+  TAddressSegment AddrSeg = { .Addr = TAddress_Max - 2, .Size = 3 };
   TAddressIterator Rator;
   TAddress Addr;
 
-
   Console.Print("( IteratorTest");
 
-  Rator.Init(1, 0);
+  if (!Rator.Init(AddrSeg))
+    Console.Print("Iterator init failed");
 
   while (Rator.GetNextAddr(&Addr))
   {
